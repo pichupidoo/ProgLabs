ff09f9a5615b809cafaac368441b33b2c47f2835
Andrew zhur <gosharak12345@mail.ru>
2023-03-02 15:35:09 +0700

    add four new files

--- lab1/p1.c
+++ lab1/p1.c
@@ -1,4 +1,25 @@
+
 #include <stdio.h>
+#include <stdlib.h>
+int *init(int *arr, int n)
 {
-	return 0;
+    arr = malloc(n * sizeof(int));
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = i;
+    }
+    return arr;
+}
+int main()
+{
+    int *arr = NULL;
+    int *mas, n = 10;
+    mas=init(arr, n);
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\n", mas[i]);
+    }
+    return 0;
 }
